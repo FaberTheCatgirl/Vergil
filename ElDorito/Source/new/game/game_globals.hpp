4d030390ec0333a7c9419a53e3e9266743acf0ab
VaktorTV <vaktortvpro@gmail.com>
2023-01-04 17:21:59 +0100

    Campaign Skulls
    by TheTwist84

--- ElDorito/Source/new/game/game_globals.hpp
+++ ElDorito/Source/new/game/game_globals.hpp
@@ -17,4 +17,10 @@ namespace blam
 	game_globals *__cdecl game_globals_get();
 
 	inline bool game_globals_simulation_has_dist_server() { return ((bool(*)())0x531AF0)(); }
+	
+	inline void game_globals_primary_skull_toggle(__int16 skull, char enable) { ((void(*)(__int16, char))0x532B50)(skull, enable); }
+	inline void game_globals_secondary_skull_toggle(__int16 skull, char enable) { ((void(*)(__int16, char))0x532EE0)(skull, enable); }
+
+	inline bool __cdecl game_globals_primary_skull_is_active(__int16 skull) { return ((bool(__cdecl*)(__int16))0x60AE20)(skull); }
+	inline bool __cdecl game_globals_secondary_skull_is_active(__int16 skull) { return ((bool(__cdecl*)(__int16))0x60AE50)(skull); }
 }
